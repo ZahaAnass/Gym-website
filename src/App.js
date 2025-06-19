b3a5e9828d6eefe013beb8987290bbe3d0d06b9e
ZahaAnass <zahaanass277@gmail.com>
2025-06-19 17:11:07 +0100

    improve the left hero section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
-      <div className="hero">
-          <div className="left-h">
-            <Header/>
-          </div>
-          <div className="right-h">
-              right side
-          </div>
-      </div>
+      <Hero/>
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,38 @@
 import React from "react"
 import "./Hero.css"
+import Header from "../Header/Header";
 
 const Hero = () => {
     return(
         <>
         <div className="hero">
-            <div className="hero-text">
-                <h1>Hero</h1>
+            <div className="left-h">
+                <Header/>
+                <div className="the-best-ad">
+                    <div className=""></div>
+                    <span>The best fitness club in the town</span>
+                </div>
+
+                <div className="hero-text">
+                    <div>
+                        <span className="stroke-text">Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal Body</span>
+                    </div>
+                    <div className="span">
+                        <span>
+                            In here we will help you to shape and 
+                            build your ideal body and live up your
+                            life to fullest
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+            <div className="right-h">
+                right side
             </div>
         </div>
         </>
