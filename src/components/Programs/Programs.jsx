b6ee51db71818d5268634eaa81322317b8d7cbd0
ZahaAnass <zahaanass277@gmail.com>
2025-07-04 15:32:29 +0100

    implement Programs component with dynamic data mapping

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import "./Programs.css"
+import { programsData } from "../../data/programsData"
+import RigthArrow from "../../assets/rightArrow.png"
 
 function Programs() {
     return (
         <>
-            <div className="">Program</div>
+            <div className="Programs" id='programs'>
+                {/* Header */}
+                <div className="programs-header">
+                    <span className='stroke-text'>Explore our</span>
+                    <span>Programs</span>
+                    <span className='stroke-text'>To shape you</span>
+                </div>
+
+                {/* Programs */}
+                <div className="programs-categories">
+                    {programsData.map((program) => (
+                        <div className="category">
+                            {program.image}
+                            <span>{program.heading}</span>
+                            <span>{program.details}</span>
+                            <div className="join-now">
+                                <span>Join Now</span>
+                                <span>
+                                    <img src={RigthArrow} alt="" />
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
