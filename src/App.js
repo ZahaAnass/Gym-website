e70e7232f723c8036de1d9998f27b70e0973a915
ZahaAnass <zahaanass277@gmail.com>
2025-07-05 13:23:01 +0100

    add the footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
 import Testimonials from './components/Testimonials/Testimonials';
 import Join from './components/Join/Join';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className='App' style={{paddingBottom: "5rem"}}>
+    <div className='App'>
       <Hero/>
       <Programs/>
       <Reasons/>
       <Plans/>
       <Testimonials/>
       <Join/>
+      <Footer/>
     </div>
   );
 }
