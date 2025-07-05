d414ee668f7cb42c576ca213d99a6561239f1c2a
ZahaAnass <zahaanass277@gmail.com>
2025-07-05 10:46:27 +0100

    add new components to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
+import Testimonials from './components/Testimonials/Testimonials';
+import Join from './components/Join/Join';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App' style={{paddingBottom: "5rem"}}>
       <Hero/>
       <Programs/>
       <Reasons/>
+      <Plans/>
+      <Testimonials/>
+      <Join/>
     </div>
   );
 }
