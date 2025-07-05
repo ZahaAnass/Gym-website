08c9d8677bdf4ffc897a8f11401cda6de192180c
ZahaAnass <zahaanass277@gmail.com>
2025-07-05 13:55:53 +0100

    implement Plans component with pricing options and features

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -6,6 +6,8 @@ import "./Plans.css"
 function Plans() {
     return (
         <div className='plans-container' id='plans'>
+            <div className="blur plans-blur-1"></div>
+            <div className="blur plans-blur-2"></div>
             <div className="programs-header" style={{gap:"2rem"}}>
                 <span className='stroke-text'>READY TO START</span>
                 <span>YOUR JOURNEY</span>
