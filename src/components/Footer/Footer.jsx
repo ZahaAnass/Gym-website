295c738d29d48e41347d1b50105fd7406cffd4a7
ZahaAnass <zahaanass277@gmail.com>
2025-07-05 13:55:53 +0100

    implement Footer component with navigation and social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,7 +20,11 @@ function Footer() {
                     <img src={Logo} alt="logo" />
                 </div>
             </div>
+
+            <div className="blur footer-blur-1"></div>
+            <div className="blur footer-blur-2"></div>
         </div>
+
     )
 }
 
